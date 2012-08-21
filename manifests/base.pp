b551047db736d42cc5d98834f6d4be5b8a46f5f6
Chris Kozak <ckozak@gmail.com>
2012-08-20 20:32:46 -0500

    Added PostGIS

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,4 @@
-package { ["python-dev", "build-essential", "python-virtualenv", "vim", "git-core", "python-jinja2", "python-setuptools", "python-nose", "gdal-bin", "mongodb-10gen"]:
+package { ["vim", "mongodb-10gen", "opengeo-postgis", "gdal-bin"]:
   require => [Exec["apt-get update"]], 
   ensure => present,
 }
@@ -6,12 +6,28 @@ package { ["python-dev", "build-essential", "python-virtualenv", "vim", "git-cor
 exec { "apt-get update":
     command => "/usr/bin/apt-get update",
     onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
-    require => File["10gen.list"]
+    require => [File["10gen.list"], File["opengeo.list"]],
 }
 
-exec { "mongo_repokey":
-    command => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
-    path    => "/usr/local/bin/:/bin/:/usr/bin/",
+exec { "restart_postgres":
+    command => "/usr/bin/touch /etc/postgresql/8.4/main/pg_hba.conf",
+    require => Package["opengeo-postgis"],
+}
+
+service { "postgresql-8.4":
+    subscribe => File["host_auth_file"],
+    ensure  => "running",
+    enable  => "true",
+    require => Package["opengeo-postgis"],
+}
+
+file { "host_auth_file":
+    path => "/etc/postgresql/8.4/main/pg_hba.conf",
+}
+
+file { "/etc/postgresql/8.4/main/pg_hba.conf.orig":
+    ensure => "absent",
+    require => Package["opengeo-postgis"],
 }
 
 file { "10gen.list":
@@ -24,3 +40,24 @@ file { "10gen.list":
     content => "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen",
     require => Exec["mongo_repokey"],
 } 
+
+file { "opengeo.list":
+    path    => "/etc/apt/sources.list.d/opengeo.list", 
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    replace => false,
+    content => "deb http://apt.opengeo.org/ubuntu lucid main",
+    require => Exec["opengeo_repokey"],
+} 
+
+exec { "opengeo_repokey":
+    command => "wget -qO- http://apt.opengeo.org/gpg.key | apt-key add -",
+    path    => "/usr/local/bin/:/bin/:/usr/bin/",
+}
+
+exec { "mongo_repokey":
+    command => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
+    path    => "/usr/local/bin/:/bin/:/usr/bin/",
+}
